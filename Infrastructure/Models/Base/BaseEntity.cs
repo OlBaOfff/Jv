c2819026d151a7abaf98e7976908933927a17f34
Tóth Olivér <t.oliver1823@stud.uni-obuda.hu>
2025-02-18 21:55:03 +0100

    BaseEntity created

---
+++ Infrastructure/Models/Base/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Base
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public bool Deleted { get; set; }
+
+
+    }
+}
