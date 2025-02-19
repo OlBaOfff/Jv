672ce9a191ddbd0570b9bae74f36a8e120c869dd
Tóth Olivér <t.oliver1823@stud.uni-obuda.hu>
2025-02-19 20:04:03 +0100

    Team class created

--- Infrastructure/Models/Ref/Referee.cs
+++ Infrastructure/Models/Ref/Referee.cs
@@ -1,5 +1,4 @@
 ﻿using Infrastructure.Models.Base;
-using Infrastructure.Models.Tester;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Infrastructure/Models/Teams/Team.cs
+++ Infrastructure/Models/Teams/Team.cs
@@ -1,5 +1,4 @@
 ﻿using Infrastructure.Models.Base;
-using Infrastructure.Models.Tester;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +7,28 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Models.Teams
 {
+    public enum Level
+    {
+        A,
+        B,
+        C,
+        D
+    }
+
+    public enum Sex
+    {
+        Leány,
+        Fiú,
+    }
+
     public class Team : BaseEntity
     {
-       
+        //Properties
+
+        public string Name { get; set; } = "";
+        public string HomeCourtName { get; set; } = "";
+        public int Age { get; set; }
+        public Sex Sex { get; set; }
+        public string WL { get; set; } = "";
     }
 }
