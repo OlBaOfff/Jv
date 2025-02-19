2cd408039de2be97fb3925c891425f27f59a58d2
Tóth Olivér <t.oliver1823@stud.uni-obuda.hu>
2025-02-19 21:21:34 +0100

    Payment created

---
+++ Infrastructure/Models/Payments/Payment.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Payments
+{
+    public enum PaymentType
+    {
+        Kézpénz,
+        Utalás,
+        Központi
+    }
+    class Payment:BaseEntity
+    {
+        //Properties
+
+        public int JvId { get; set; }
+        public int Amount { get; set; }
+        public List<int> MatchesId { get; set; } = new List<int>();
+        public PaymentType PaymentType { get; set; }
+    }
+}
