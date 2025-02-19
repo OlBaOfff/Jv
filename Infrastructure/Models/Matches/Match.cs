174eb4d7402dccdba7318e77c712c697dd3e987d
Tóth Olivér <t.oliver1823@stud.uni-obuda.hu>
2025-02-19 20:15:15 +0100

    Match class created

---
+++ Infrastructure/Models/Matches/Match.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Matches
+{
+    public enum Level
+    {
+        Easy,
+        Medium,
+        Hard,
+        Veteran
+
+    }
+    
+    class Match:BaseEntity
+    {
+        //Properties
+
+        public string Location { get; set; } = "";
+        public DateTime Start { get; set; }
+        public Level Difficulty { get; set; }
+        public int Fee { get; set; }
+        public string Colleagues { get; set; } = "";
+        public int HomeTeamId { get; set; }
+        public int GuestTeamId { get; set; }
+    }
+}
