f42c3cc17c1a6ef008cf38cf49a59b2a4ab8796f
Tóth Olivér <t.oliver1823@stud.uni-obuda.hu>
2025-02-19 12:26:54 +0100

    Referee class created

---
+++ Infrastructure/Models/Ref/Referee.cs
@@ -0,0 +1,44 @@
+﻿using Infrastructure.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Ref
+{
+    public class Referee:BaseEntity
+    {
+        //fields
+
+        private int id;
+        private string name;
+        private string address;
+        private List<DateTime> workTime;
+        private string rang;
+        private bool licence;
+        private List<string> info;
+
+        //ctor
+        public Referee(int id, string name, string address, List<DateTime> workTime, string rang, bool licence, List<string> info)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Address = address;
+            this.WorkTime = workTime;
+            this.Rang = rang;
+            this.Licence = licence;
+            this.Info = info;
+        }
+
+        //Properties
+        
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Address { get => address; set => address = value; }
+        public List<DateTime> WorkTime { get => workTime; set => workTime = value; }
+        public string Rang { get => rang; set => rang = value; }
+        public bool Licence { get => licence; set => licence = value; }
+        public List<string> Info { get => info; set => info = value; }
+    }
+}
