0278de8fff846a4534df2efecc7ea9b239780cc5
Tóth Olivér <t.oliver1823@stud.uni-obuda.hu>
2025-03-14 22:20:07 +0100

    MatchRepository was created

---
+++ Infrastructure/Repositories/MatchRepository.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Models.Matches;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class MatchRepository : BaseRepository<Match>
+    {
+        public MatchRepository(Infrastructure.DbCont.DbCont context) : base(context)
+        {
+
+        }
+
+        public override IQueryable<Match> WithInClude()
+        {
+            return base.WithInClude()
+                .Include(x => x.Teams)
+                .Include(y => y.Referees);
+        }
+    }
+}
